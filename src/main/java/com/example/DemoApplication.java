63c294d17a2917a381c263b9cd962954c1919652
LPF <1527831187@qq.com>
2019-03-08 11:42:25 +0800

    加入actuactor监控中心，adminui监控中心图像化，依赖引入外加名称配置 @EnableAdminServer

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+/*@SpringBootApplication*/
+@EnableAdminServer
 @SpringBootApplication
 public class DemoApplication{
 
